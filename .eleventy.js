c53aebb268faa88ab3aa75588de10b7bd390c037
Nicolas Hoizey <nicolas@hoizey.com>
2020-11-17 23:55:33 +0100

    Add the images-responsiver plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,18 @@
 module.exports = function (eleventyConfig) {
-  return {
-    templateFormats: ['md', 'njk'],
+  
+  // Make simple images responsive for production
+  if (process.env.NODE_ENV === 'production') {
+    const imagesResponsiver = require('eleventy-plugin-images-responsiver');
+    eleventyConfig.addPlugin(imagesResponsiver, {
+      
+    });
+  }
 
-    markdownTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
+  eleventyConfig
+    .addPassthroughCopy('src/images')
+    .addPassthroughCopy('src/styles.css');
+
+  return {
     passthroughFileCopy: true,
     dir: {
       output: 'dist',
